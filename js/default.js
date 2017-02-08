a15380f6378b594b116140d4755c2bb6694b1456
Priscila <Priscila>
2017-02-07 23:34:41 -0200

    Initial commit

---
+++ js/default.js
@@ -0,0 +1,32 @@
+$(function () {
+    $("#includedHeader").load("header.html");
+    $("#includedNavigation").load("navigation.html");
+    $("#includedFooter").load("footer.html");
+    $("#includedQualidade").load("qualidades.html");
+});
+
+$(document).ready(function () {
+    $.getScript("js/dropdown-menu.js", function (data, textStatus, jqxhr) {
+        console.log(data); // Data returned
+        console.log(textStatus); // Success
+        console.log(jqxhr.status); // 200
+        //console.log("Load was performed.");
+    });
+
+    $.getScript("js/sticky-header.js", function (data, textStatus, jqxhr) {
+        console.log(data); // Data returned
+        console.log(textStatus); // Success
+        console.log(jqxhr.status); // 200
+        //console.log("Load was performed.");
+    });
+
+    var url = window.location.href;
+    var urlSplit = url.split("/");
+    var pagina = urlSplit[urlSplit.length - 1];
+    pagina = pagina.replace("#", "")
+
+    var currentLink = $('a[href*="' + pagina + '"]');
+    var liParent = $('a[href*="' + pagina + '"]').closest('li[class^="parent"]');
+    //alert(liParent);
+    $('a[href*="' + pagina + '"]').closest('li[class^="parent"]').addClass("active");
+});
---
+++ js/location-single.js
@@ -0,0 +1,32 @@
+// A $( document ).ready() block.
+$(document).ready(function () {
+
+
+    var myCenter = new google.maps.LatLng(-30.0292644, -51.2204043);
+
+    function initialize() {
+        var mapProp = {
+            center: myCenter,
+            zoom: 14,
+            scrollwheel: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+        var marker = new google.maps.Marker({
+            position: myCenter,
+            icon: 'images/marker.png'
+        });
+
+        marker.setMap(map);
+
+        var infowindow = new google.maps.InfoWindow({
+            content: "Servipac"
+        });
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+});
+
